dfb4e01bb2e645ab153f4c26d29baedbee8bbc49
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-20 16:39:36 -0500

    Resolviendo erroes y probando la aplicación de suma
    Resolviendo erroes y probando la aplicación de suma.

--- proyecto003/src/App.js
+++ proyecto003/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form onSubmit={calcularSuma}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ingrese primer valor: 
+          <input type="number" name="valor1" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Ingrese segundo valor: 
+          <input type="number" name="valor2" />
+        </p>
+        <p>
+          <input type="submit" value="Sumar" />
+        </p>
+      </form>
     </div>
   );
+}
+
+function calcularSuma(event) {
+  event.preventDefault();
+
+  const valor1 = parseInt(event.target.valor1.value, 10);
+  const valor2 = parseInt(event.target.valor2.value, 10);
+
+  const suma = valor1 + valor2;
+
+  alert('La suma es: ' + suma);
 }
 
 export default App;
