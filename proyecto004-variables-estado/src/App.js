054b3481c9f7dfde4570896f3f7928d57b9cf4c1
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-21 16:06:03 -0500

    Corrección del problema de referencia de variable
    Corrección del problema de referencia de variable,

--- proyecto004-variables-estado/src/App.js
+++ proyecto004-variables-estado/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [numero, setNumero] = useState(0);
+
+  function generarEnteroAleatorio() {
+    const enteroAleatorio = Math.trunc(Math.random() * 10);
+    setNumero(enteroAleatorio);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <p>Número aleatorio: {numero}</p>
+      <button onClick={generarEnteroAleatorio}>Genenerar entero aleatorio</button>    </div>
   );
 }
 
