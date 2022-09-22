a11f2f778af8e1bc34814235a84449c547ddc6db
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-22 16:42:45 -0500

    Cambio de título con un useEffect
    Cambio de título con un useEffect,

--- proyecto008-hook-efecto/src/App.js
+++ proyecto008-hook-efecto/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [texto, setTexto] = useState('');
+
+  useEffect(() => {
+    document.title = texto;
+  }, [texto]);
+
+  function cambiar(e) {
+    setTexto(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <input type="text" onChange={cambiar}/>
+      </p>
+      <p>{texto}</p>
     </div>
   );
 }
