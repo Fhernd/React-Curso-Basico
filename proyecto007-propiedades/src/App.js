d360ee8f16f134be5314c5e3693244a3ad399975
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-22 15:11:49 -0500

    Creación del JSX para demostra la calculadora
    Creación del JSX para demostra la calculadora.

--- proyecto007-propiedades/src/App.js
+++ proyecto007-propiedades/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import {useState} from 'react';
+import ListadoResultados from './ListadoResultados';
 
 function App() {
   const [operaciones, setOperacion] = useState([]);
@@ -23,7 +24,20 @@ function App() {
     event.target.valor2.value = '';
   }
 
-  return ();
+  return (
+    <div>
+      <form onSubmit={sumar}>
+        <p>
+          Ingrese primer valor: <input type="number" name="valor1" />
+        </p>
+        <p>
+          Ingrese segundo valor: <input type="number" name="valor2" />
+        </p>
+        <input type="submit" value="Sumar" />
+      </form>
+      <ListadoResultados resultados={operaciones} />
+    </div>
+  );
 }
 
 export default App;
