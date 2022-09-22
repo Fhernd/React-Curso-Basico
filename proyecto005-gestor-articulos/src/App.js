71334a53248c1b246160497966aa797ba6620926
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-22 10:15:04 -0500

    Finalizado el proyecto de eliminación de artículos
    Finalizado el proyecto de eliminación de artículos.

--- proyecto005-gestor-articulos/src/App.js
+++ proyecto005-gestor-articulos/src/App.js
@@ -2,6 +2,23 @@ import { useState } from 'react';
 
 import './App.css';
 
+
+function generarFila(articulo) {
+  return (
+    <tr key={articulo.codigo}>
+      <td>
+        {articulo.codigo}
+      </td>
+      <td>
+        {articulo.descripcion}
+      </td>
+      <td>
+        {articulo.precio}
+      </td>
+    </tr>
+  );
+}
+
 function App() {
   function eliminarUltimafila() {
     if (articulos.length) {
@@ -30,7 +47,21 @@ function App() {
   ]);
 
   return (
-    
+    <div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articulos.map(generarFila)}
+        </tbody>
+      </table>
+      <button onClick={eliminarUltimafila}>Eliminar última fila</button>
+    </div>
   );
 }
 
