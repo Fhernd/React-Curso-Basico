73d7f9925c3a8d7b717937f6c0836a97331fbd31
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-23 15:40:47 -0500

    Cada fila tiene una llave (key) distinta.
    Cada fila tiene una llave (key) distinta.

--- proyecto011-propiedad-key/src/App.js
+++ proyecto011-propiedad-key/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function borrar(codigo) {
+    const temporal = articulos.filter(e => e.codigo !== codigo);
+    setArticulos(temporal);
+  }
+
+  const [articulos, setArticulos] = useState([
+    {
+      codigo: 1,
+      descripcion: 'papas',
+      precio: 12.52
+    }, {
+      codigo: 2,
+      descripcion: 'naranjas',
+      precio: 21
+    }, {
+      codigo: 3,
+      descripcion: 'peras',
+      precio: 18.20
+    }
+  ]);
+
+  return(
+    <div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+            <th>¿Borrar?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articulos.map(e => {
+            return(
+              <tr key={e.codigo}>
+                <td>{e.codigo}</td>
+                <td>{e.descripcion}</td>
+                <td>{e.precio}</td>
+                <td>
+                  <button onClick={() => borrar(e.codigo)}>Borrar</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
