ae1b535545d0a889541b1aa83e8b83bca7d5f843
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-24 07:15:09 -0500

    Sección JSX para el formulario y asociación de hooks de estado
    Sección JSX para el formulario y asociación de hooks de estado.

--- proyecto012-formularios/src/App.js
+++ proyecto012-formularios/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [datos, setDatos] = useState({
+    nombre: '',
+    edad: 0,
+    estudios: false
+  });
+
+  function cambiarNombre(event) {
+    setDatos(valores => ({
+      ...valores,
+      nombre: event.target.value
+    }));
+  }
+
+  function cambiarEdad(event) {
+    setDatos(valores => ({
+      ...valores,
+      edad: event.target.value
+    }));
+  }
+
+  function cambiarEstudio(event) {
+    setDatos(valores => ({
+      ...valores,
+      estudios: !datos.estudios
+    }));
+  }
+
+  function procesar(event) {
+    event.preventDefault();
+
+    alert(`Dato cargado: Nombre: ${datos.nombre} - Edad: ${datos.edad} - Estudios: ${datos.estudios}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form onSubmit={procesar}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ingrese nombre: <input type="text" value={datos.nombre} onChange={cambiarNombre}/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Ingrese edad: <input type="number" value={datos.edad} onChange={cambiarEdad}/>
+        </p>
+        <p>
+          Estudios: <input type="checkbox" value={datos.estudios} onChange={cambiarEstudio}/>
+        </p>
+        <p>
+          <input type="submit"/>
+        </p>
+      </form>
+      <hr/>
+      <h3>Datos ingresados</h3>
+      <p>Nombre: {datos.nombre}</p>
+      <p>Edad: {datos.edad}</p>
+      <p>Estudios: {datos.estudios ? 'con estudios' : 'sin estaudios'}</p>
     </div>
   );
 }
