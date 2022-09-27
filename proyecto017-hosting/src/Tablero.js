e0b5182a2635dd13b29f6ed0c741bc0ea9105556
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-27 13:59:42 -0500

    Segmento JSX para retornar un tablero y sus casillas
    Segmento JSX para retornar un tablero y sus casillas.

--- proyecto017-hosting/src/Tablero.js
+++ proyecto017-hosting/src/Tablero.js
@@ -7,18 +7,18 @@ import './Tablero.css';
 function Tablero() {
     const [tablero, setTablero] = useState(
         [
-            ['\u265C', '\u265E', '\u265D','\u265A', '\u265A', '\u265D', '\u265E', '\u265C'],
-            ['\u265F', '\u265F', '\u265F','\u265F', '\u265F', '\u265F', '\u265F', '\u265F'],
-            ['', '', '','', '', '', '', ''],
-            ['', '', '','', '', '', '', ''],
-            ['', '', '','', '', '', '', ''],
-            ['', '', '','', '', '', '', ''],
-            ['\u2659', '\u2659', '\u2659','\u2659', '\u2659', '\u2659', '\u2659', '\u2659'],
-            ['\u2656', '\u2658', '\u2657','\u2655', '\u2654', '\u2657', '\u2658', '\u2656']
+            ['\u265C', '\u265E', '\u265D', '\u265A', '\u265A', '\u265D', '\u265E', '\u265C'],
+            ['\u265F', '\u265F', '\u265F', '\u265F', '\u265F', '\u265F', '\u265F', '\u265F'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['\u2659', '\u2659', '\u2659', '\u2659', '\u2659', '\u2659', '\u2659', '\u2659'],
+            ['\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657', '\u2658', '\u2656']
         ]
     );
 
-    useState( () => {
+    useState(() => {
         function drag(event) {
             event.dataTransfer.setData('fila', event.target.attributes.fila.value);
             event.dataTransfer.setData('columna', event.target.attributes.columna.value);
@@ -39,7 +39,7 @@ function Tablero() {
 
             for (let f = 0; f < 8; ++f) {
                 for (let c = 0; c < 8; ++c) {
-                    if(parseInt(event.target.attributes.fila.value) === f && parseInt(pevent.target.attributes.columna.value) === c) {
+                    if (parseInt(event.target.attributes.fila.value) === f && parseInt(pevent.target.attributes.columna.value) === c) {
                         nuevoTablero[f][c] = valor;
 
                         if (f !== fila || c !== columna) {
@@ -71,8 +71,12 @@ function Tablero() {
     }, [tablero]);
 
     return (
-        <div>
-            Tablero
+        <div className="tablero">
+            {tablero.map((fila, i) => {
+                return fila.map((casilla, j) => {
+                    return (<Casilla valor={casilla} fila={i} columna={j} key={i + j} pieza={casilla} color={(i + j) % 2 === 0 ? 'blanco' : 'negro'} />);
+                })
+            })}
         </div>
     );
 }
