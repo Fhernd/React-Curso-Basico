e7ddb99110baf28a5964bf0a2cdca41b6e4b7c10
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-28 15:29:41 -0500

    Creado el arreglo para contener los nombres de los archivos de banderas
    Creado el arreglo para contener los nombres de los archivos de banderas.

--- proyecto018-carpeta-public/src/App.js
+++ proyecto018-carpeta-public/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const banderasPaises = ['argentina.png', 'bolivia.png', 'brasil.png', 'chile.png', 'colombia.png', 'peru', 'uruguay', 'venezuela'];
+
+  const [banderaSeleccionada, setBanderaSeleccionada] = React.useState(4);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
