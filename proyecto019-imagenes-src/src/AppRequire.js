661214f966eb587f365cf017a59701b5450c1e9c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-29 16:55:40 -0500

    Prueba de uso de imágenes con require()
    Prueba de uso de imágenes con require().

--- proyecto019-imagenes-src/src/AppRequire.js
+++ proyecto019-imagenes-src/src/AppRequire.js
@@ -1,35 +1,26 @@
 import { useState } from 'react';
 import './App.css';
 
-import argentina from './imagenes/argentina.png';
-import bolivia from './imagenes/bolivia.png';
-import brasil from './imagenes/brasil.png';
-import chile from './imagenes/chile.png';
-import colombia from './imagenes/colombia.png';
-import peru from './imagenes/peru.png';
-import uruguay from './imagenes/uruguay.png';
-import venezuela from './imagenes/venezuela.png';
-
 function App() {
-  const banderas = [argentina, brasil, bolivia, chile, colombia, peru, uruguay, venezuela];
-  const [numeroBandera, setNumeroBandera] = useState(4);
+  const banderas = ['argentina.png', 'brasil.png', 'bolivia.png', 'chile.png', 'colombia.png', 'peru.png', 'uruguay.png', 'venezuela.png'];
+  const [indice, setIndice] = useState(4);
 
   function banderaSiguiente() {
-    if (numeroBandera < banderas.length - 1) {
-      setNumeroBandera(numeroBandera + 1);
+    if (indice < banderas.length - 1) {
+      setIndice(indice + 1);
     }
   }
 
   function banderaAnterior() {
-    if (numeroBandera > 0) {
-      setNumeroBandera(numeroBandera - 1);
+    if (indice > 0) {
+      setIndice(indice - 1);
     }
   }
 
   return (
     <div className="App">
       <h1>Banderas de países latinoamericanos</h1>
-      <p><img src={banderas[numeroBandera]} alt="Bandera" /></p>
+      <p><img src={require(`./imagenes/${banderas[indice]}`)} alt="Bandera" /></p>
       <p>
         <input type="button" value="<" onClick={banderaAnterior} />
         <input type="button" value=">" onClick={banderaSiguiente} />
