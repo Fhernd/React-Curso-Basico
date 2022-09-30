1e7297324c0744132da9c3cd3a9e5f6298bf0ad9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-30 15:26:38 -0500

    Archivo para contener los datos de rubros y artículos
    Archivo para contener los datos de rubros y artículos. Sobre un archivo JavaScript.

---
+++ proyecto020-select-dependiente/src/datos.js
@@ -0,0 +1,66 @@
+const rubros = [{
+    codigo: 1,
+    nombre: 'Microprocesadores'
+},
+{
+    codigo: 2,
+    nombre: 'Tarjetas de video'
+},
+{
+    codigo: 3,
+    nombre: 'Gabinetes'
+}
+]
+
+const articulos = [{
+    codigo: 1,
+    codigoRubro: 1,
+    nombre: 'Microprocesador Intel Core i5',
+    precio: 1000
+},
+{
+    codigo: 2,
+    codigoRubro: 1,
+    nombre: 'Microprocesador Intel Core i7',
+    precio: 2000
+},
+{
+    codigo: 3,
+    codigoRubro: 1,
+    nombre: 'Microprocesador Intel Core i9',
+    precio: 3000
+},
+{
+    codigo: 4,
+    codigoRubro: 2,
+    nombre: 'Tarjeta de video MSI GeForce GTX 1080',
+    precio: 790
+},
+{
+    codigo: 5,
+    codigoRubro: 2,
+    nombre: 'Tarjeta de video MSI GeForce GTX 1060',
+    precio: 1200
+},
+{
+    codigo: 6,
+    codigoRubro: 2,
+    nombre: 'Tarjeta de video MSI GeForce GTX 1050',
+    precio: 1800
+},
+{
+    codigo: 7,
+    codigoRubro: 3,
+    nombre: 'Gabinete MSI',
+    precio: 840
+},
+{
+    codigo: 8,
+    codigoRubro: 3,
+    nombre: 'Gabinete Cooler Master 200 Black',
+    precio: 1120
+}
+]
+
+export { articulos }
+export { rubros }
